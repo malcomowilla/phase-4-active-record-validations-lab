6a5db42917ec2566e03b360e7bbf0a3d24d2fc88
malcomowilla <owillamalcom@gmail.com>
2023-06-24 15:10:32 +0300

    l

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in:["Fiction," "Non-Fiction"] }
+    validate :title_contains_believe
+end
+
+def title_contains_believe
+  catch_phrases = ["Top [number]", "Won't Believe", "Secret", "Guess"]
+
+  unless catch_phrases.any? { |phrase| title.include?(phrase) }
+    errors.add(:title, "should be sufficiently clickbait-y")
+  end
 end
